576949f52fdf14ac8c868c824216ecfa7ef43651
sorange59 <153960527+sorange59@users.noreply.github.com>
2024-07-09 16:55:29 -0700

    Nicer looks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
     private
     def authenticate_user!
         if session[:user_id].nil?
-        redirect_to user_path, alert: 'You must be logged in to access this page'
         end
     end
 end
